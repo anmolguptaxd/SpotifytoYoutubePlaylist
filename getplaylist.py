855535475d358057fc65a292c5d659e224e0a19e
Anmol Gupta <anmolgutaxd@gmail.com>
2023-11-04 16:54:17 +0530

    Added function to import the playlists from spotify to youtube

--- getplaylist.py
+++ getplaylist.py
@@ -1,180 +1,210 @@
 from flask import Flask, request, redirect, session, url_for, render_template
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
 import os
 import uuid
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
-from oauth2client.client import flow_from_clientsecrets
-from oauth2client.file import Storage
-from oauth2client.tools import run_flow
+import google.auth
+import requests
+import os
+from google.oauth2.credentials import Credentials
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+
 
 app = Flask(__name__)
 
-# Set the environment variables in code
-os.environ['SPOTIPY_CLIENT_ID'] = '3ccee5fed52f4719957a790531670859'
-os.environ['SPOTIPY_CLIENT_SECRET'] = '849030335b9b4f89b1f61820e6d1aaf5'
-os.environ['SPOTIPY_REDIRECT_URI'] = 'http://127.0.0.1:5000/callback'
+# Configure your Spotify and YouTube credentials
+SPOTIFY_CLIENT_ID = '3ccee5fed52f4719957a790531670859'
+SPOTIFY_CLIENT_SECRET = '849030335b9b4f89b1f61820e6d1aaf5'
+SPOTIFY_REDIRECT_URI = 'http://127.0.0.1:5000/callback'
+YOUTUBE_CLIENT_SECRETS_FILE = "client-secret.json"
+YOUTUBE_SCOPES = ['https://www.googleapis.com/auth/youtube']
 
-# Generate a random secret key - this should be kept secret in production
 app.secret_key = str(uuid.uuid4())
 
-# Configure your Spotify credentials
-app.config['SPOTIPY_CLIENT_ID'] = '3ccee5fed52f4719957a790531670859'
-app.config['SPOTIPY_CLIENT_SECRET'] = '849030335b9b4f89b1f61820e6d1aaf5'
-app.config['SPOTIPY_REDIRECT_URI'] = 'http://127.0.0.1:5000/callback'
-app.config['SCOPE'] = 'playlist-read-private'
+# The Spotify scope needed to read private playlists
+SPOTIFY_SCOPE = 'playlist-read-private'
 
-# Helper function to get the current cache path
+# The session cache path for the Spotify authentication token
 def session_cache_path():
     cache_directory = '.spotify_caches'
-    if not os.path.isdir(cache_directory):
+    if not os.path.exists(cache_directory):
         os.makedirs(cache_directory)
-    return os.path.join(cache_directory, session.get('uuid', ''))
+    return os.path.join(cache_directory, session.get('uuid'))
+
+# Helper function to create YouTube OAuth flow
+def create_youtube_flow():
+    flow = InstalledAppFlow.from_client_secrets_file(
+        YOUTUBE_CLIENT_SECRETS_FILE, scopes=YOUTUBE_SCOPES)
+    # This sets the redirect URI for the flow
+    flow.redirect_uri = YOUTUBE_REDIRECT_URI
+    return flow
+
+# Helper function to get YouTube client
+def get_youtube_client():
+    creds_data = session.get('credentials')
+    
+    if not creds_data:
+        return None
+    
+    # Construct credentials from the creds_data using the correct class method
+    credentials = Credentials.from_authorized_user_info(creds_data)
+    
+    return build('youtube', 'v3', credentials=credentials)
+
 
 @app.route('/')
 def index():
-    cache_handler = spotipy.cache_handler.CacheFileHandler(cache_path=session_cache_path())
-    auth_manager = SpotifyOAuth(
-        client_id=app.config['SPOTIPY_CLIENT_ID'],
-        client_secret=app.config['SPOTIPY_CLIENT_SECRET'],
-        redirect_uri=app.config['SPOTIPY_REDIRECT_URI'],
-        scope=app.config['SCOPE'],
-        cache_handler=cache_handler,
-        show_dialog=True
-    )
-
+    # Check if user has a session UUID, if not create one
     if not session.get('uuid'):
-        # Step 2. User is not logged in, generate a random session UUID
         session['uuid'] = str(uuid.uuid4())
 
+    # Spotify OAuth2 flow
+    cache_handler = spotipy.cache_handler.CacheFileHandler(cache_path=session_cache_path())
+    auth_manager = SpotifyOAuth(
+        client_id=SPOTIFY_CLIENT_ID,
+        client_secret=SPOTIFY_CLIENT_SECRET,
+        redirect_uri=SPOTIFY_REDIRECT_URI,
+        scope=SPOTIFY_SCOPE,
+        cache_handler=cache_handler,
+        show_dialog=True
+)
+
+    # If there is no token in the cache for the user, redirect to Spotify Auth
     if not auth_manager.validate_token(cache_handler.get_cached_token()):
-        # Step 4. Display sign in link when no token
         auth_url = auth_manager.get_authorize_url()
         return render_template('index.html', auth_url=auth_url)
 
-    # Step 5. Signed in, display playlists
-    spotify = spotipy.Spotify(auth_manager=auth_manager)
-    return export_playlists(spotify)
+    # If user is authenticated with Spotify but not with YouTube, redirect to YouTube Auth
+    if 'credentials' not in session:
+        return redirect(url_for('login_youtube'))
+
+    # If user is authenticated with both, redirect to show playlists
+    return redirect(url_for('show_playlists'))
 
 @app.route('/callback')
 def callback():
-    # Step 6. Being redirected from Spotify auth page with the code
     cache_handler = spotipy.cache_handler.CacheFileHandler(cache_path=session_cache_path())
     auth_manager = SpotifyOAuth(
-        client_id=app.config['SPOTIPY_CLIENT_ID'],
-        client_secret=app.config['SPOTIPY_CLIENT_SECRET'],
-        redirect_uri=app.config['SPOTIPY_REDIRECT_URI'],
-        scope=app.config['SCOPE'],
+        client_id=SPOTIFY_CLIENT_ID,
+        client_secret=SPOTIFY_CLIENT_SECRET,
+        redirect_uri=SPOTIFY_REDIRECT_URI,
+        scope=SPOTIFY_SCOPE,
         cache_handler=cache_handler,
         show_dialog=True
     )
-    # Exchange code for token
-    auth_manager.get_access_token(request.args.get("code"))
-    return redirect(url_for('index'))
 
-@app.route('/logout')
-def logout():
-    # Remove the CACHE file so that a new user can authorize.
-    try:
-        os.remove(session_cache_path())
-    except OSError as e:
-        print(f"Error: {e.filename} - {e.strerror}.")
-    session.clear()
-    return redirect('/')
+    # Process the Spotify callback and redirect to YouTube login if needed
+    auth_manager.get_access_token(request.args.get('code'))
+    return redirect(url_for('login_youtube'))
 
-def export_playlists(spotify):
-    # Fetch user playlists
-    playlists = spotify.current_user_playlists()
-    data = []
+@app.route('/login_youtube')
+def login_youtube():
+    flow = create_youtube_flow()
+    # Generate a state token for CSRF protection
+    state = str(uuid.uuid4())
+    session['state'] = state
+    authorization_url, state = flow.authorization_url(
+        access_type='offline', 
+        include_granted_scopes='true',
+        state=state
+    )
+    return redirect(authorization_url)
 
-    for playlist in playlists['items']:
-        if playlist['owner']['id'] == spotify.me()['id']:
-            # Get all tracks in the playlist
-            results = spotify.playlist_tracks(playlist['id'])
-            tracks = results['items']
-            while results['next']:
-                results = spotify.next(results)
-                tracks.extend(results['items'])
-            
-            # Format playlist data
-            playlist_data = {
-                'name': playlist['name'],
-                'tracks': [f"{track['track']['name']} by {', '.join([artist['name'] for artist in track['track']['artists']])}" for track in tracks]
-            }
+YOUTUBE_REDIRECT_URI = 'http://127.0.0.1:5000/youtube_callback'
+@app.route('/youtube_callback')
+def youtube_callback():
+    # State validation should be implemented here
+    state = session.get('state')
+    if not state or state != request.args.get('state'):
+        return "State does not match!", 400
+    flow = create_youtube_flow()
+    flow.fetch_token(authorization_response=request.url)
 
-            data.append(playlist_data)
-            
-# Within export_playlists() after fetching Spotify playlists
-    youtube = get_youtube_client()
-    for playlist_data in data:
-        yt_playlist_id = create_youtube_playlist(youtube, playlist_data['name'], 'Imported from Spotify')
-        for track in playlist_data['tracks']:
-             track_name, track_artists = track.split(" by ")
-             add_track_to_youtube_playlist(youtube, yt_playlist_id, track_name, track_artists)
+    if not flow.credentials:
+        return "Failed to authenticate with YouTube", 400
 
-    # You would typically render a template here
-    return render_template('playlists.html', playlists=data)
+    session['credentials'] = credentials_to_dict(flow.credentials)
+    # Store the credentials in the session
     
+
+    return redirect(url_for('show_playlists'))
+
+def credentials_to_dict(credentials):
+    return {
+        'token': credentials.token,
+        'refresh_token': credentials.refresh_token,
+        'token_uri': credentials.token_uri,
+        'client_id': credentials.client_id,
+        'client_secret': credentials.client_secret,
+        'scopes': credentials.scopes,
+    }
+
+@app.route('/show_playlists')
+def show_playlists():
+    # Get Spotify client
+    cache_handler = spotipy.cache_handler.CacheFileHandler(cache_path=session_cache_path())
+    spotify = spotipy.Spotify(auth_manager=SpotifyOAuth(cache_handler=cache_handler))
+    
+    # Fetch Spotify playlists
+    spotify_playlists = spotify.current_user_playlists(limit=10)
+
+    # Now, let's create YouTube playlists and add tracks
+    youtube = get_youtube_client()
+    if not youtube:
+        return "Failed to get YouTube client", 400
+
+    for sp_playlist in spotify_playlists['items']:
+        # Create a new YouTube playlist with the same name as the Spotify playlist
+        create_playlist_response = youtube.playlists().insert(
+            part="snippet,status",
+            body=dict(
+                snippet=dict(
+                    title=sp_playlist['name'],
+                    description="Created from Spotify playlist"
+                ),
+                status=dict(
+                    privacyStatus="private"
+                )
+            )
+        ).execute()
+        
+        youtube_playlist_id = create_playlist_response['id']
+        
+        # Fetch tracks from the Spotify playlist
+        sp_tracks = spotify.playlist_tracks(sp_playlist['id'])
+        for track in sp_tracks['items']:
+            track_name = track['track']['name']
+            artist_name = track['track']['artists'][0]['name']
+            
+            # Search for the track on YouTube
+            search_response = youtube.search().list(
+                q=f"{artist_name} {track_name}",
+                part="snippet",
+                maxResults=1,
+                type="video"
+            ).execute()
+
+            if search_response['items']:
+                youtube_video_id = search_response['items'][0]['id']['videoId']
+                
+                # Add the video to the playlist
+                youtube.playlistItems().insert(
+                    part="snippet",
+                    body=dict(
+                        snippet=dict(
+                            playlistId=youtube_playlist_id,
+                            resourceId=dict(
+                                kind="youtube#video",
+                                videoId=youtube_video_id
+                            )
+                        )
+                    )
+                ).execute()
+
+    # Return the list of Spotify playlists and a success message for YouTube operation
+    return render_template('playlists.html', playlists=spotify_playlists['items'], message="Playlists created on YouTube.")
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-YOUTUBE_CLIENT_SECRETS_FILE = "14lbiv5trssmjsj05gnjfdo6mvmitlta.apps.googleusercontent.com.json"
-YOUTUBE_SCOPES = ["https://www.googleapis.com/auth/youtube.force-ssl"]
-def get_youtube_client():
-    flow = flow_from_clientsecrets(YOUTUBE_CLIENT_SECRETS_FILE, scope=YOUTUBE_SCOPES)
-    storage = Storage("%s-oauth2.json" % sys.argv[0])
-    credentials = storage.get()
-
-    if credentials is None or credentials.invalid:
-        flags = argparser.parse_args()
-        credentials = run_flow(flow, storage, flags)
-
-    return build("youtube", "v3", credentials=credentials)
-
-def create_youtube_playlist(youtube, title, description):
-    playlists_insert_response = youtube.playlists().insert(
-        part="snippet,status",
-        body=dict(
-            snippet=dict(
-                title=title,
-                description=description
-            ),
-            status=dict(
-                privacyStatus="private"
-            )
-        )
-    ).execute()
-
-    return playlists_insert_response["id"]
-
-def add_track_to_youtube_playlist(youtube, playlist_id, track_name, track_artists):
-    # Search for the track on YouTube
-    search_response = youtube.search().list(
-        q=f"{track_name} {track_artists}",
-        part="id,snippet",
-        maxResults=1,
-        type="video"
-    ).execute()
-
-    videos = search_response.get("items", [])
-    if not videos:
-        print(f"No video found for {track_name} by {track_artists}.")
-        return
-
-    # Add the first search result to the playlist
-    video_id = videos[0]["id"]["videoId"]
-    add_video_request = youtube.playlistItems().insert(
-        part="snippet",
-        body={
-            "snippet": {
-                "playlistId": playlist_id,
-                "resourceId": {
-                    "kind": "youtube#video",
-                    "videoId": video_id
-                }
-            }
-        }
-    ).execute()
-
